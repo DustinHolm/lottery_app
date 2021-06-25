ab22a2ffe22ec4b1b6336c0793b63a851e00048b
Dennis <degei002@stud.fh-dortmund.de>
2021-06-25 20:12:06 +0200

    Updated the createNewProductPage

--- lib/pages/create_new_product_page.dart
+++ lib/pages/create_new_product_page.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:lottery_app/models/checkBoxState.dart';
 import 'package:lottery_app/sidebar.dart';
 
 class CreateNewProductPage extends StatefulWidget {
   CreateNewProductPage({Key? key}) : super(key: key);
   final String title = 'Angebot erstellen';
-  String enteredProductName = '';
+  //String enteredProductName = '';
+  //String enteredProductDescription = '';
+  //String enteredProductDetails = '';
 
   @override
   _CreateNewProductPageState createState() => _CreateNewProductPageState();
 }
 
 class _CreateNewProductPageState extends State<CreateNewProductPage> {
+//A Controller to get the TextField Value from Multiline TextFields
+//A onSubmit function in a Multiline TextFields does not work
+  final textFieldControllerProductName = TextEditingController();
+  final textFieldControllerProductDescription = TextEditingController();
+  final textFieldControllerProductDetails = TextEditingController();
+
+  //Generate Checkboxes for the Categories
+  final categorieList = [
+    //dummy data for testing
+    CheckBoxState(title: 'Elektroartikel'),
+    CheckBoxState(title: 'Küchenzubehör'),
+    CheckBoxState(title: 'Haushaltsgeräte'),
+  ];
+
+  @override
+  void dispose() {
+    // cleans up the controller after use
+    textFieldControllerProductName.dispose();
+    textFieldControllerProductDescription.dispose();
+    textFieldControllerProductDetails.dispose();
+    super.dispose();
+  }
+
+  Widget buildSingleCheckbox(CheckBoxState checkbox) => CheckboxListTile(
+    controlAffinity: ListTileControlAffinity.leading,
+    value: checkbox.value,
+    title: Text(
+      checkbox.title,
+    ),
+    onChanged: (value) => setState(() => checkbox.value = value!),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,72 +53,143 @@ class _CreateNewProductPageState extends State<CreateNewProductPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Fügen Sie ihrem Produkt ein Bild hinzu'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 5, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Fügen Sie ihrem Produkt ein Bild hinzu'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage(
+                            'assets/placeholder_for_product_image.png'),
+                        width: 220,
+                        height: 170,
+                      ),
+                      IconButton(
+                        onPressed: () => {
+                          //TODO: after pressing the button, open the phones camera and replace the placeholder with the chosen image
+                          print('Camera IconButton was pressed')
+                        },
+                        iconSize: 50.0,
+                        icon: Icon(Icons.camera_alt),
+                      ),
+                      IconButton(
+                        onPressed: () => {
+                          //TODO: after pressing the button, open the phones folders/gallery and replace the placeholder with the chosen image
+                          print('Folder IconButton was pressed')
+                        },
+                        iconSize: 50.0,
+                        icon: Icon(Icons.folder),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ), //For Picture and Picture Select Buttons
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 5, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Geben Sie ihrem Produkt einen Name'),
+                  Container(
+                    //width: 300.0,
+                    child: TextField(
+                      //TODO: styling InputDialog, looking more into async functions
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        //labelText: 'Produktame',
+                      ),
+                      controller: textFieldControllerProductName,
+
+                    ),
+                  ),
+                ],
+              ),
+            ), //For the Product Name Text Field
+
+            Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 5, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Image(
-                      image: AssetImage(
-                          'assets/placeholder_for_product_image.png'),
-                      width: 220,
-                      height: 170,
+                    Text('Geben Sie ihrem Produkt eine Beschreibung'),
+                    Container(
+                      child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                          ),
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 7,
+                          controller: textFieldControllerProductDescription,
                     ),
-                    IconButton(
-                      onPressed: () => {
-                        //TODO: after pressing the button, open the phones camera and replace the placeholder with the chosen image
-                        print('Camera IconButton was pressed')
-                      },
-                      iconSize: 50.0,
-                      icon: Icon(Icons.camera_alt),
-                    ),
-                    IconButton(
-                      onPressed: () => {
-                        //TODO: after pressing the button, open the phones folders/gallery and replace the placeholder with the chosen image
-                        print('Folder IconButton was pressed')
-                      },
-                      iconSize: 50.0,
-                      icon: Icon(Icons.folder),
                     ),
                   ],
-                ),
-              ],
-            ),
-          ), //For Picture and Picture Select Buttons
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Geben Sie ihrem Produkt einen Name'),
-                Container(
-                  //width: 300.0,
-                  child: TextField(        //TODO: styling InputDialog, looking more into async functions
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      //labelText: 'Produktame',
+                )), //Product Description
+            Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 5, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Wählen Sie passende Kategorien aus'),
 
+                    ...categorieList.map(buildSingleCheckbox).toList(),  // '...' is here the 'spread' operator
+                  ],
+                ),
+            ), //Select a Product Category
+
+            Container(
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+
+                      margin: EdgeInsets.fromLTRB(0, 0, 5, 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Geben Sie Details über ihr Produkt an'),
+                          TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                              ),
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 4,
+                              controller: textFieldControllerProductDetails,
+                          ),
+                        ],
+                      )),
+                  Container(
+                    // Button to send Information further to Backend/ Server
+                    child: TextButton(
+                      child: Text('Inserieren'),
+                      onPressed: () => {
+                        print('Inserieren Button gedrückt'),
+                        print(textFieldControllerProductName.text),
+                        print(textFieldControllerProductDescription.text),
+                        print(textFieldControllerProductDetails.text),
+
+                        print(categorieList[0].value),
+                      },
 
                     ),
-                    onSubmitted: (String value) => {
-                      widget.enteredProductName = value,  //get the text from textfield
-                      print(widget.enteredProductName),
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ), //For the Product Name Text Field
 
-          Row(), //For the Product Description
-          Row(), //For the Product category
-          Row(), //For the Product details and submit button
-        ],
+                    ),
+                ],
+              ),
+            ), //
+          ],
+        ),
       ),
     );
   }
+
 }
