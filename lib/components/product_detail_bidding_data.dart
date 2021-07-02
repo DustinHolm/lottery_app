d7d667721356985be2eb7f5d346414c00f5092ae
Dustin <dustin.holm@gmx.net>
2021-07-02 12:08:04 +0200

    Worked more on the Shipping display

--- lib/components/product_detail_bidding_data.dart
+++ lib/components/product_detail_bidding_data.dart
@@ -22,47 +22,49 @@ class ProductDetailBiddingData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Countdown countdown = Countdown(
-      seconds: getTimeLeft(),
-      build: (context, time) {
-        Duration duration = Duration(seconds: time.toInt());
-        int days = duration.inDays;
-        int hours = duration.inHours;
-        int minutes = duration.inMinutes;
-        int seconds = duration.inSeconds;
-        if (days > 1) {
-          return Text(
-            "$days Tage",
-            style: Theme.of(context).textTheme.headline3,
-            textAlign: TextAlign.center,
-          );
-        } else if (hours > 1) {
-          return Text(
-            "$hours h",
-            style: Theme.of(context).textTheme.headline3,
-            textAlign: TextAlign.center,
-          );
-        } else if (minutes > 1) {
-          return Text(
-            "$minutes min",
-            style: Theme.of(context).textTheme.headline3,
-            textAlign: TextAlign.center,
-          );
-        } else if (seconds > 0) {
-          return Text(
-            "$seconds s",
-            style: Theme.of(context).textTheme.headline3,
-            textAlign: TextAlign.center,
-          );
-        } else {
-          return Text(
-            "--",
-            style: Theme.of(context).textTheme.headline3,
-            textAlign: TextAlign.center,
-          );
-        }
-      },
-    );
+    Countdown renderCountdown() {
+      return Countdown(
+        seconds: getTimeLeft(),
+        build: (context, time) {
+          Duration duration = Duration(seconds: time.toInt());
+          int days = duration.inDays;
+          int hours = duration.inHours;
+          int minutes = duration.inMinutes;
+          int seconds = duration.inSeconds;
+          if (days > 1) {
+            return Text(
+              "$days Tage",
+              style: Theme.of(context).textTheme.headline3,
+              textAlign: TextAlign.center,
+            );
+          } else if (hours > 1) {
+            return Text(
+              "$hours h",
+              style: Theme.of(context).textTheme.headline3,
+              textAlign: TextAlign.center,
+            );
+          } else if (minutes > 1) {
+            return Text(
+              "$minutes min",
+              style: Theme.of(context).textTheme.headline3,
+              textAlign: TextAlign.center,
+            );
+          } else if (seconds > 0) {
+            return Text(
+              "$seconds s",
+              style: Theme.of(context).textTheme.headline3,
+              textAlign: TextAlign.center,
+            );
+          } else {
+            return Text(
+              "--",
+              style: Theme.of(context).textTheme.headline3,
+              textAlign: TextAlign.center,
+            );
+          }
+        },
+      );
+    }
 
     return Container(
         padding: EdgeInsets.all(8),
@@ -85,7 +87,7 @@ class ProductDetailBiddingData extends StatelessWidget {
           ),
           Expanded(
             child: GridTile(
-              child: countdown,
+              child: renderCountdown(),
               footer: Text(
                 "Verbleibende Zeit",
                 style: Theme.of(context).textTheme.overline,
--- lib/components/product_detail_seller_data.dart
+++ lib/components/product_detail_seller_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lottery_app/components/shipping_icon.dart';
 import 'package:lottery_app/enums/collect_type.dart';
 import 'package:lottery_app/models/user.dart';
 
@@ -13,18 +14,58 @@ class ProductDetailSellerData extends StatelessWidget {
   CollectType collectType;
   int shippingCost;
 
+  String getCollectTypeString() {
+    switch (collectType) {
+      case CollectType.SELF_COLLECT:
+        return "Nur Selbstabholung";
+      case CollectType.PACKET:
+        return "Lieferung möglich";
+      case CollectType.PACKET_INLAND:
+        return "Lieferung möglich (Inland)";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    GridTile renderShipping() {
+      if (collectType == CollectType.SELF_COLLECT) {
+        return GridTile(
+          child: ShippingIcon(
+            collectType: collectType,
+          ),
+          footer: Text(
+            getCollectTypeString(),
+            style: Theme.of(context).textTheme.overline,
+            textAlign: TextAlign.center,
+          ),
+        );
+      } else {
+        return GridTile(
+          child: Text(
+            "$shippingCost Tickets",
+            style: Theme.of(context).textTheme.headline3,
+            textAlign: TextAlign.center,
+          ),
+          footer: Row(children: [
+            ShippingIcon(
+              collectType: collectType,
+            ),
+            Text(
+              getCollectTypeString(),
+              style: Theme.of(context).textTheme.overline,
+              textAlign: TextAlign.center,
+            ),
+          ]),
+        );
+      }
+    }
+
     return Container(
       padding: EdgeInsets.all(8),
       height: 80,
       child: Row(
         children: [
-          Expanded(
-              child: GridTile(
-            child: Text("Versandgebühr: ${shippingCost.toString()}"),
-            footer: Text("Versandart: ${collectType.toString()}"),
-          )),
+          Expanded(child: renderShipping()),
           Expanded(
               child: GridTile(
             child: Text("${seller.name}"),
