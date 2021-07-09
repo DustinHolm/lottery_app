65c2b6ce0578c59e7e388322250fe9d3645f5e06
ich <malte.b.weiss@gmail.com>
2021-07-09 20:04:34 +0200

    change welcome page add notifyParent to UserDialog

--- lib/components/user_dialog.dart
+++ lib/components/user_dialog.dart
@@ -1,65 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:lottery_app/stores/user_store.dart';
+import 'package:provider/provider.dart';
 
 class UserDialog extends StatelessWidget {
-  const UserDialog({Key? key}) : super(key: key);
+  final String buttonText;
+  final Function notifyParent;
+
+  UserDialog({this.buttonText = 'icon', Function? notifyParent, Key? key})
+      : notifyParent = notifyParent ?? (() => {}),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     UserStore userStore = context.read<UserStore>();
 
-    return IconButton(
-        icon: const Icon(Icons.account_circle),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return SimpleDialog(
-                  title: Text(
-                    "Nutzer",
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                  children: [
-                    ListTile(
-                        title: (userStore.status == Status.AUTHENTICATED)
-                            ? Text(userStore.name)
-                            : Text("Nicht bei Google angemeldet")),
-                    if (userStore.status == Status.AUTHENTICATED)
-                      ListTile(
-                        title: Text("${userStore.tickets} Tickets"),
-                      ),
-                    Container(
-                        padding: EdgeInsets.all(8),
-                        child: userStore.status == Status.WAITING
-                            ? CircularProgressIndicator()
-                            : userStore.status == Status.UNAUTHENTICATED
-                                ? ElevatedButton(
-                                    onPressed: () async =>
-                                        await userStore.signIn().then((_) {
-                                      Navigator.pop(context);
-                                    }),
-                                    child: Text("Mit Google anmelden"),
-                                  )
-                                : ElevatedButton(
-                                    onPressed: () async =>
-                                        await userStore.signOut().then((_) {
-                                      Navigator.pop(context);
-                                    }),
-                                    child: Text("Abmelden"),
-                                  )),
-                    Container(
-                        padding: EdgeInsets.all(8),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            userStore.status = Status.AUTHENTICATED;
-                            Navigator.pop(context);
-                          },
-                          child: Text("Fakeanmeldung"),
-                        )),
-                  ]);
+    SimpleDialog dialogFn(context) => SimpleDialog(
+            title: Text(
+              "Nutzer",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            children: [
+              ListTile(
+                  title: (userStore.status == Status.AUTHENTICATED)
+                      ? Text(userStore.name)
+                      : Text("Nicht bei Google angemeldet")),
+              if (userStore.status == Status.AUTHENTICATED)
+                ListTile(
+                  title: Text("${userStore.tickets} Tickets"),
+                ),
+              Container(
+                  padding: EdgeInsets.all(8),
+                  child: userStore.status == Status.WAITING
+                      ? CircularProgressIndicator()
+                      : userStore.status == Status.UNAUTHENTICATED
+                          ? ElevatedButton(
+                              onPressed: () async =>
+                                  await userStore.signIn().then((_) {
+                                notifyParent();
+                                Navigator.pop(context);
+                              }),
+                              child: Text("Mit Google anmelden"),
+                            )
+                          : ElevatedButton(
+                              onPressed: () async {
+                                return await userStore.signOut().then((_) {
+                                  notifyParent(); //TODO funktioniert nicht
+                                  Navigator.pop(context);
+                                });
+                              },
+                              child: Text("Abmelden"),
+                            )),
+              Container(
+                  padding: EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      userStore.status = Status.AUTHENTICATED;
+                      notifyParent();
+                      Navigator.pop(context);
+                    },
+                    child: Text("Fakeanmeldung"),
+                  )),
+            ]);
+
+    return buttonText == 'icon'
+        ? IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return dialogFn(context);
+                },
+              );
+            })
+        : TextButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return dialogFn(context);
+                },
+              );
             },
-          );
-        });
+            child: Text(buttonText));
   }
 }
--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,30 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:lottery_app/components/app_bar.dart';
+import 'package:lottery_app/components/user_dialog.dart';
 import 'package:lottery_app/services/local_storage_service.dart';
+import 'package:lottery_app/stores/user_store.dart';
+import 'package:provider/provider.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
+  WelcomePage({Key? key}) : super(key: key);
   final String title = "Lotteriespiel-App";
 
   @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  String text0 =
+      "Bevor du auf Produkte bieten kannst musst du dir einen Account anlegen.";
+  String text1 = "Wenn du dies jetzt tun willst, klicke auf";
+  String text2 = "Möchtest du die App erstmal testen, klicke auf";
+  String text3 = "als Gast fortfahren";
+  bool signIn = true;
+
+  @override
   Widget build(BuildContext context) {
+    UserStore userStore = context.read<UserStore>();
+
+    if (userStore.status == Status.AUTHENTICATED) {
+      text0 = "Erfolgreich angemeldet.";
+      text1 = "Account wechseln";
+      text2 = "Klicken zum Fortfahren";
+      text3 = "fortfahren";
+      signIn = false;
+    }
+
     return Scaffold(
-      appBar: lotteryAppBar(title),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                "Willkommen, du bist willkommen auf der Willkommensseite unserer tollen Lotterie-App!",
-                style: Theme.of(context).textTheme.subtitle1,
+              SizedBox(
+                  child: Image.asset("assets/logo.png"),
+                  width: 200,
+                  height: 200),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  "Willkommen!",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
               ),
-              ElevatedButton(
-                  onPressed: () async {
-                    await LocalStorageService.setIsFirstUse(false);
-                    Navigator.popAndPushNamed(context, "/overview");
-                  },
-                  child: Text("Weiter")),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  children: [
+                    Text(
+                      "Du hast soeben zum ersten Mal die Lotteriespiel-App geöffnet.",
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Text(
+                      text0,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: Text(
+                            text1,
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ),
+                        UserDialog(
+                            buttonText: signIn ? 'anmelden' : 'wechseln',
+                            notifyParent: () => setState(() {})),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: Text(
+                            text2,
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ),
+                        TextButton(
+                            onPressed: () async {
+                              await LocalStorageService.setIsFirstUse(false);
+                              Navigator.popAndPushNamed(context, "/overview");
+                            },
+                            child: Text(text3))
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
