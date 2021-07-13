1141a81f17f37d14f678ca94226a2d30f5245376
Dustin <dustin.holm@gmx.net>
2021-07-13 18:32:41 +0200

    Added logo as Android icon

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Lotteriespiel-App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
