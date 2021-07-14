7d9c141d58977a544846d5c23c0c48b50981c38d
Dustin <dustin.holm@gmx.net>
2021-07-14 21:52:01 +0200

    Removed import

--- lib/filter/i_transform.dart
+++ lib/filter/i_transform.dart
@@ -1,7 +1,5 @@
-import 'package:lottery_app/enums/filter.dart';
 import 'package:lottery_app/models/lottery.dart';
 
 abstract class ITransform {
   Iterable<Lottery> transformLotteries(Iterable<Lottery> lotteries);
-  Filter? getEnum();
 }
